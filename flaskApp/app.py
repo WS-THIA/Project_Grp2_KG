14704ab610df5396adfe6a926189e9dfcdd080c0
kevin-moe <48941665+kevin-moe@users.noreply.github.com>
2019-11-23 15:16:05 +0800

    updated Flask App and included word2vec

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect
-from forms import textInterface, radioInterface
+from forms import textInterface, radioInterface, recommendationInterface
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'myKey123'
@@ -7,12 +7,18 @@ app.config['SECRET_KEY'] = 'myKey123'
 ###### Function here ######
 def QueryFunction(food_text):
 
-    # query here
-
-    # convert to data frame
 
     #return the dataframe
     return food_text + ' testing'
+
+def Cuisine_query(cuisine_txt):
+
+
+    return cuisine_txt + str(12345)
+
+def makeRecommendation(userID):
+
+    return userID + str(123)
 
 ###### End Function ######
 
@@ -23,20 +29,36 @@ def main():
 
     form2 = radioInterface()
 
+    form3 = recommendationInterface()
+
     if form1.validate_on_submit():
+
         food = form1.textInput.data
 
         # Call function here
         data = QueryFunction(food)
+        return render_template('reviews.html', data=data)
 
-        return render_template('reviews.html', data = data)
+    if form2.validate_on_submit():
 
-    return render_template('index.html', form1=form1, form2=form2)
+        cuisine = form2.cuisine.data
 
-@app.route('/reviews')
-def reviews():
+        data = Cuisine_query(cuisine)
 
-    return render_template('reviews.html')
+        return render_template('reviews.html', data=data)
+
+    if form3.validate_on_submit():
+
+        userID = form3.userID.data
+
+        userID = 'U' + userID
+
+        data = makeRecommendation(userID)
+
+        return render_template('reviews.html', data=data)
+
+    return render_template('index.html', form1=form1, form2=form2, form3=form3)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flaskApp/forms.py
+++ flaskApp/forms.py
@@ -1,18 +1,28 @@
-from wtforms import StringField, SubmitField, RadioField
+from wtforms import StringField, SubmitField, RadioField, TextAreaField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 
 class textInterface(FlaskForm):
 
     textInput = StringField('What would you like to eat??', validators=[DataRequired()])
+
     submitText = SubmitField('Feed ME!')
 
 class radioInterface(FlaskForm):
 
-    Gender = RadioField('Gender', choices = [('chinese','Chinese'),
-                                             ('japanese','Japanese'),
-                                             ('local','Local'),
-                                             ('italian','Italian'),
-                                             ('french','French')], validators=[DataRequired()])
+    cuisine= RadioField('cuisine', choices = [('Asian','Asian'),
+                                             ('Japanese','Japanese'),
+                                             ('Westerm','Western'),
+                                             ('Dessert','Dessert'),
+                                             ('Western-Local','Western-Local'),
+                                            ('Japanese-Local','Japanese-Local'),
+                                            ('Western-Japanese','Western-Japanese')], validators=[DataRequired()])
 
     submitRadio = SubmitField('Feed ME!')
+
+class recommendationInterface(FlaskForm):
+
+    userID = StringField('Who would you like to make a recommendation to?', validators=[DataRequired()])
+
+    submitRecommend = SubmitField("Recommend!")
+
