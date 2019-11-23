9e7f1410a5851f839d49e9d288b5feb3865a1b2c
zjyiwing <56811895+zjyiwing@users.noreply.github.com>
2019-11-23 21:30:55 +0800

    upload backend

---
+++ data/readme.md
@@ -0,0 +1,7 @@
+## About 
+
+`sample_scraped.json` contains the scraped data of a single user.
+`sample_users_scraped.json` contains about 544 users' scraped data. 
+This is incomplete; @ testing stage now. 
+
+
---
+++ flaskApp/forms.py
@@ -0,0 +1,34 @@
+from wtforms import StringField, SubmitField, RadioField, TextAreaField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+
+class textInterface(FlaskForm):
+
+    textInput = StringField('What would you like to eat??', validators=[DataRequired()])
+
+    sortBy = RadioField('Order by...', choices = [('number_of_reviews','Number of Reviews'),('rating','Rating')],
+                        validators=[DataRequired()])
+
+    submitText = SubmitField('Feed ME!')
+
+class radioInterface(FlaskForm):
+
+    cuisine= RadioField('Which cuisine would you like to explore?', choices = [('Asian','Asian'),
+                                             ('Japanese','Japanese'),
+                                             ('Westerm','Western'),
+                                             ('Dessert','Dessert'),
+                                             ('Western-Local','Western-Local'),
+                                            ('Japanese-Local','Japanese-Local'),
+                                            ('Western-Japanese','Western-Japanese')], validators=[DataRequired()])
+
+    sortBy = RadioField('Order by...', choices = [('number_of_reviews','Number of Reviews'),('rating','Rating')],
+                        validators=[DataRequired()])
+
+    submitRadio = SubmitField('Feed ME!')
+
+class recommendationInterface(FlaskForm):
+
+    userID = StringField('Who would you like to make a recommendation to?', validators=[DataRequired()])
+
+    submitRecommend = SubmitField("Recommend!")
+
