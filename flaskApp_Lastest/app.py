bd160cfb62307d17bac19a0256f63ba546b2753b
zjyiwing <56811895+zjyiwing@users.noreply.github.com>
2019-11-26 21:48:07 +0800

    update to latest

--- flaskApp_Lastest/app.py
+++ flaskApp_Lastest/app.py
@@ -35,17 +35,17 @@ def r_recommender(graph, cid, num_nearest_neighbors, num_recommendations):
               COLLECT(re.name)[0..{n}] as recommendations  
            """
 
-    recommendations = {}
+
 
     for i in graph.run(query, p_name=cid, k=num_nearest_neighbors, n=num_recommendations):
-        recommendations[i[0]] = i[1]
+
+        recommendations=i[1]
 
     return recommendations
 
 
 def search_restaurant_by_cusine(graph, text, sort):
     result = []
-    result_review = {}
 
     query_rating = """
         MATCH (r:Review)-[a:About]->(re:Restaurant)
@@ -65,26 +65,6 @@ def search_restaurant_by_cusine(graph, text, sort):
         ORDER BY number DESC
 
     """
-
-    query_top = """
-            MATCH (r:Review)-[a:About]->(re:Restaurant)
-            WHERE re.name ={term}
-            WITH re, a, r
-            ORDER BY a.restaurant_sentiment DESC
-            WITH re, COLLECT(r)[0..3] AS top3
-            UNWIND top3 AS top
-            RETURN re.name, top.review
-            """
-
-    query_bottom = """
-            MATCH (r:Review)-[a:About]->(re:Restaurant)
-            WHERE re.name = {term}
-            WITH re, a, r
-            ORDER BY a.restaurant_sentiment DESC
-            WITH re, COLLECT(r)[-4..-1] AS bottom3
-            UNWIND bottom3 AS bottom
-            RETURN re.name, bottom.review
-            """
     if sort == 'number_of_reviews':
         query = query_popularity
 
@@ -92,32 +72,111 @@ def search_restaurant_by_cusine(graph, text, sort):
         query = query_rating
 
     ans = graph.run(query, term=text).data()
-    if not ans:
+
+    for item in ans:
+        result.append(item['re.name'])
+
+    if not result:
         return ['No records found']
     else:
-        for item in ans:
-            result.append(item['re.name'])
-            result_review[item['re.name']] = []
+        return result
 
-        for res in result:
-            t = []
-            b = []
 
-            temp_t = graph.run(query_top, term=res).data()
-            for element in temp_t:
-                result_review[element['re.name']].append(element['top.review'])
+def query_reviews_cuisine(graph, res_list):
+    result = []
+    query_top = """
+                MATCH (r:Review)-[a:About]->(re:Restaurant)
+                WHERE re.name = {term}
+                WITH re, a, r
+                ORDER BY a.restaurant_sentiment DESC
+                WITH re, COLLECT(r)[0..3] AS top3
+                UNWIND top3 AS top
+                RETURN re.name, top.review
+                """
 
-            temp_b = graph.run(query_bottom, term=res).data()
-            for element in temp_b:
-                result_review[element['re.name']].append(element['bottom.review'])
+    query_bottom = """
+                MATCH (r:Review)-[a:About]->(re:Restaurant)
+                WHERE re.name = {term}
+                WITH re, a, r
+                ORDER BY a.restaurant_sentiment DESC
+                WITH re, COLLECT(r)[-4..-1] AS bottom3
+                UNWIND bottom3 AS bottom
+                RETURN re.name, bottom.review
+                """
+    for item in res_list:
 
-        return result_review
+        temp = [item]
+        tmp_t = graph.run(query_top, term=item).data()
+        for r in tmp_t:
+            if not r['top.review']:
+                temp.append('No reviews')
+            else:
+                temp.append(r['top.review'])
+
+        tmp_b = graph.run(query_bottom, term=item).data()
+        for r in tmp_b:
+            if not r['bottom.review']:
+                temp.append('No reviews')
+            else:
+                temp.append(r['bottom.review'])
+
+        result.append(temp)
+
+    return result
+
+
+def query_reviews_food(graph, res_list, food):
+    result = []
+    query_top = """
+            MATCH (r:Review)-[a:About]->(re:Restaurant)
+            WHERE re.name = {restaurant}
+            WITH re, r
+            MATCH (r:Review)-[c:Contains]->(f:Food)
+            WHERE f.name = {food}
+            WITH re, r, c
+            ORDER BY c.food_sentiment DESC
+            WITH re, COLLECT(r)[0..3] AS top3
+            UNWIND top3 AS top
+            RETURN re.name, top.review
+            """
+
+    query_bottom = """
+            MATCH (r:Review)-[a:About]->(re:Restaurant)
+            WHERE re.name = {restaurant}
+            WITH re, r
+            MATCH (r:Review)-[c:Contains]->(f:Food)
+            WHERE f.name = {food}
+            WITH re, r, c
+            ORDER BY c.food_sentiment DESC
+            WITH re, COLLECT(r)[-4..-1] AS bottom3
+            UNWIND bottom3 AS bottom
+            RETURN re.name, bottom.review
+            """
+    for item in res_list:
+
+        temp = [item]
+        tmp_t = graph.run(query_top, restaurant=item, food=food).data()
+        for r in tmp_t:
+            if not r['top.review']:
+                temp.append('No reviews')
+            else:
+                temp.append(r['top.review'])
+
+        tmp_b = graph.run(query_bottom, restaurant=item, food=food).data()
+        for r in tmp_b:
+            if not r['bottom.review']:
+                temp.append('No reviews')
+            else:
+                temp.append(r['bottom.review'])
+
+        result.append(temp)
+
+    return result
 
 
 def search_restaurant_by_food(graph, text, sort):
     result = []
     key_words = text
-    result_review = {}
 
     query_rating = """
         MATCH (r:Review)-[c:Contains]->(f:Food)
@@ -136,32 +195,6 @@ def search_restaurant_by_food(graph, text, sort):
         ORDER BY number DESC
         """
 
-    query_top = """
-            MATCH (r:Review)-[a:About]->(re:Restaurant)
-            WHERE re.name = {restaurant}
-            WITH re, a, r
-            MATCH (r:Review)-[c:Contains]->(f:Food)
-            WHERE f.name = {food}
-            WITH re, r, c
-            ORDER BY c.food_sentiment DESC
-            WITH re, COLLECT(r)[0..3] AS top3
-            UNWIND top3 AS top
-            RETURN re.name, top.review
-            """
-
-    query_bottom = """
-                MATCH (r:Review)-[a:About]->(re:Restaurant)
-                WHERE re.name = {restaurant}
-                WITH re, a, r
-                MATCH (r:Review)-[c:Contains]->(f:Food)
-                WHERE f.name = {food}
-                WITH re, r, c
-                ORDER BY c.food_sentiment DESC
-                WITH re, COLLECT(r)[-4..-1] AS bottom3
-                UNWIND bottom3 AS bottom
-                RETURN re.name, bottom.review
-                """
-
     if sort == 'number_of_reviews':
         query = query_popularity
 
@@ -173,68 +206,42 @@ def search_restaurant_by_food(graph, text, sort):
     if ans:
         for item in ans:
             result.append(item['re.name'])
-            result_review[item['re.name']] = []
+        return result
 
-        for res in result:
-            t = []
-            b = []
-
-            temp_t = graph.run(query_top, restaurant=res, food=key_words).data()
-            for element in temp_t:
-                result_review[element['re.name']].append(element['top.review'])
-
-            temp_b = graph.run(query_bottom, restaurant=res, food=key_words).data()
-            for element in temp_b:
-                result_review[element['re.name']].append(element['bottom.review'])
-
-        return result_review
-
-    if not ans:
+    else:
         key_words = key_words.split()
-        result = {}
+        result = []
 
         for i in range(len(key_words)):
             temp = graph.run(query, term=key_words[i]).data()
-            result[key_words[i]] = []
             for item in temp:
-                result[key_words[i]].append(item['re.name'])
+                result.append(item['re.name'])
 
         if not result:
             return ['No records found']
         else:
-            for res in result:
-                t = []
-                b = []
-
-                temp_t = graph.run(query_top, restaurant=res, food=key_words).data()
-                for element in temp_t:
-                    result_review[element['re.name']].append(element['top.review'])
-
-                temp_b = graph.run(query_bottom, restaurant=res, food=key_words).data()
-                for element in temp_b:
-                    result_review[element['re.name']].append(element['bottom.review'])
-
-            return result_review
+            return result
 
 
 ###### Function here ######
 def QueryFunction(food_text, sortBy):
     food_text = food_text.lower()
     res = search_restaurant_by_food(burpple, food_text, sortBy)
+    result = query_reviews_food(burpple,res,food_text)
 
-    return res
+    return result
 
 
 def Cuisine_query(cuisine_txt, sortBy):
-    print(cuisine_txt)
     cuisine_txt = cuisine_txt.lower()
     res = search_restaurant_by_cusine(burpple, cuisine_txt,sortBy)
+    result = query_reviews_cuisine(burpple, res)
 
-    return res
+    return result
 
 
 def makeRecommendation(userID):
-    print(userID)
+
     res = r_recommender(burpple, userID, 10, 5)
 
     return res
@@ -274,7 +281,7 @@ def main():
 
         data = makeRecommendation(userID)
 
-        return render_template('reviews.html', data=data)
+        return render_template('recommend.html', data=data)
 
     return render_template('index.html', form1=form1, form2=form2, form3=form3)
 
